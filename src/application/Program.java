604bfc9f562ab38525f902cfa3965c6ff19c32ce
Caio Lamarão <caiolamarao@outlook.com>
2022-06-29 15:46:03 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Books");
+		Department dep1 = new Department(1, "Books");
 		
-		System.out.println(dep);
+		System.out.println(dep1);
+		
+		Seller seller = new Seller(1, "Caio", "caiol5@hotmail.com", new Date(), 3000.00, dep1);
+		
+		System.out.println(seller);
 
 	}
 
