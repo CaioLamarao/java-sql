757f44fa30759041a1d52325d3bbf074a9e93cb3
Caio Lamarão <caiolamarao@outlook.com>
2022-06-29 16:21:37 -0300

    SellerDaoJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		System.out.println(dep1);
 		
 		Seller seller = new Seller(1, "Caio", "caiol5@hotmail.com", new Date(), 3000.00, dep1);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
