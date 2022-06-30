22141573aeb4dc7e68c1cb4207192499283193d1
Caio Lamarão <caiolamarao@outlook.com>
2022-06-30 14:37:50 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep1 = new Department(1, "Books");
-		
-		System.out.println(dep1);
-		
-		Seller seller = new Seller(1, "Caio", "caiol5@hotmail.com", new Date(), 3000.00, dep1);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
